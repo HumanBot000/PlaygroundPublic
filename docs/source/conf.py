90447ff35da78d5ba229c0a0b453832a559ad9c7
HumanBot000 <tobias.sarosi@gmail.com>
2023-10-21 12:38:08 +0200

    added features.md

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Playground'
+copyright = '2023,Playground Dev Team'
+author = 'Tobias'
 
 release = '0.1'
 version = '0.1.0'
